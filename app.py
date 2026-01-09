f6a8f62d11e2c14c53ad1ef61d87f7b0274df2eb
itzJishanAhmad09 <itzjishanahmad09@gmail.com>
2026-01-09 16:12:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ def prepare_model():
         return movies, similarity
 
     # LOAD LOCAL FILES (NO DOWNLOAD)
-    movies = pd.read_csv("tmdb_5000_movies.csv.zip")
-    credits = pd.read_csv("tmdb_5000_credits.csv.zip")
+   movies = pd.read_csv("tmdb_5000_movies.csv.zip", compression="zip")
+   credits = pd.read_csv("tmdb_5000_credits.csv.zip", compression="zip")
+
 
     movies = movies.merge(credits, on="title")
     movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
@@ -88,4 +89,5 @@ def recommend(movie):
 if st.button("Recommend"):
     for movie in recommend(selected_movie):
         st.write("👉", movie)
+
 
